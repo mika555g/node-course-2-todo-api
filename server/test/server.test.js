1341119068a3f4441adc50dc53f6c34d5374161c
unknown <mikagarm@gmail.com>
2018-09-25 10:48:36 +0400

    Add test cases for get todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,12 +1,14 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectID,
     text: 'First test todo'
 }, {
+    _id: new ObjectID,
     text: 'Second test todo'
 }]
 
@@ -69,3 +71,32 @@ describe('GET /todos', () => {
          .end(done);
     });
 });
+
+describe('GET /todos/:id', () => {
+    it('it should return todo doc', (done) => {
+        request(app)
+         .get(`/todos/${todos[0]._id.toHexString()}`)
+         .expect(200)
+         .expect( (res) => {
+             expect(res.body.todo.text).toBe(todos[0].text);
+         })
+         .end(done);
+    });
+
+    it('it should 404 return todo not found', (done) => {
+        var hexId = new ObjectID().toHexString();
+
+        request(app)
+         .get(`/todos/${hexId}`)
+         .expect(404)
+         .end(done);
+    });
+
+    it('it should 404 return todo not-object ids', (done) => {
+        request(app)
+         .get(`/todos/123abc`)
+         .expect(404)
+         .end(done);
+    }); 
+});
+
